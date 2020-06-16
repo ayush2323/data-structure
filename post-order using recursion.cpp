4b2cddaabe77499f91cb013bbbba20d1be8ccbb8
Ayush Tiwari <62030895+ayush2323@users.noreply.github.com>
2020-06-16 06:12:12 +0100

    Create post-order using recursion.cpp

---
+++ post-order using recursion.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+  public:
+  int data;
+  Node *left;
+  Node *right;
+
+  Node(int data)
+  {
+    this -> data = data;
+    left = right = NULL;
+  }
+};
+
+void postOrder(Node *root)
+{
+  if(root == NULL)
+  {
+    return;
+  }
+  postOrder(root -> left);
+  postOrder(root -> right);
+  cout<<root -> data<<" ";  
+}
+
+int main() 
+{
+  Node *root = new Node(1);
+  root -> left = new Node(2);
+  root -> right = new Node(3);
+  root -> left -> left = new Node(4);
+  root -> left -> right = new Node(5);
+  root -> right -> left = new Node(6);
+  root -> right -> right = new Node(7); 
+  
+  postOrder(root);
+}
