6170deeb88a4ea856d991a7d0d119cf51ddaeef9
Ayush Tiwari <62030895+ayush2323@users.noreply.github.com>
2020-06-20 09:44:03 +0100

    Create sum of array.cpp

---
+++ sum of array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a[5] = {10,20,30,40,50};
+  int sum = 0;
+
+  for(int i = 0; i < 5; i++)
+  {
+    sum = sum + a[i];
+  }
+  cout<<"sum of given arrray is: "<<sum<<endl;
+}
