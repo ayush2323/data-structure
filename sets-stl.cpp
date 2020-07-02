21e09bec37fb38021a2a82c01bc7b7945cb6cd2b
Ayush Tiwari <62030895+ayush2323@users.noreply.github.com>
2020-07-02 04:34:42 +0100

    Update sets-stl.cpp

--- sets-stl.cpp
+++ sets-stl.cpp
@@ -8,17 +8,65 @@ using namespace std;
 int main() {
   set<int> s;
 
-  s.insert(1); // adding data to set
-  s.insert(2);
-  s.insert(3);
-  s.insert(-1);
-  s.insert(-10);
+  s.insert(40); // adding data to set
+  s.insert(30);
+  s.insert(60);
+  s.insert(20);
+  s.insert(50);
+  s.insert(50); // duplicate data cant be addded
+  s.insert(10);
 
-  for(int x : s)
+  set<int, greater<int>> :: iterator itr;
+  cout<<"\n the set s is : ";
+
+  // printing the data
+  for(itr = s.begin(); itr != s.end(); itr++)
   {
-    cout<< x <<" ";
+    cout<< '\t' <<*itr;
   }
   cout<<endl;
+
+  // assigning the elements from s to s1
+  set <int> s1(s.begin(), s.end());
+
+  // print all the elements of the set s1
+  cout<<"\n the set s1 is : ";
+
+  // printing the data
+  for(itr = s.begin(); itr != s.end(); itr++)
+  {
+    cout<< '\t' <<*itr;
+  }
+  cout<<endl;
+
+  // remove all elements upto 30 in s1
+  cout<<"\ns1 after removal of elements less than 30: ";
+  s1.erase(s1.begin(), s1.find(30));
+  for(int x : s1) // other way of writing iterator
+  {
+    cout<<x<<" ";
+  }
+  cout<<endl;
+
+  // remove element with value 50 in s1
+  int num;
+  num = s1.erase(50);
+  cout<<"\ns1.erase(50) : ";
+  cout<<num<<" removed \t";
+
+  for(int x : s1)
+  {
+    cout<<x<<" ";
+  }
+  cout<<endl;
+
+  // lower bound and upper bound for set s
+  cout<<"s.lower_bound(40) : "<<*s.lower_bound(40)<<endl;
+  cout<<"s.upper_bound(40) : "<<*s.upper_bound(40)<<endl;
+
+  // lower bound and upper bound for set s1
+  cout<<"s1.lower_bound(40) : "<<*s1.lower_bound(40)<<endl;
+  cout<<"s1.upper_bound(40) : "<<*s1.upper_bound(40)<<endl;
 
   auto it = s.find(-1); // find the data and pointing to that position
 
@@ -32,8 +80,8 @@ int main() {
    cout<<"data is "<<*it<<endl; 
   }
 
-  auto it2 = s.lower_bound(-1);
-  auto it3 = s.upper_bound(-1);
+  auto it2 = s.lower_bound(30);
+  auto it3 = s.upper_bound(30);
   cout<<"data is "<<*it2<<" and data is "<<*it3<<endl; 
 
   auto it4 = s.upper_bound(3);
@@ -47,8 +95,6 @@ int main() {
    cout<<"data is present"<<endl;
    cout<<"data is "<<*it<<endl; 
   }
-  
-  s.erase(2); // deleting the data
 
   it = s.begin(); // delete the first data
   ++it; // delete the next data
